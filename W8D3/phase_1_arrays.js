c4fb9a6e9a6475582e9f78ecd4321fe72db55127
Brandon Leong <bjtl1105@gmail.com>
2020-09-16 18:04:13 -0700

    JS Stuff

---
+++ W8D3/phase_1_arrays.js
@@ -0,0 +1,57 @@
+// Array.prototype.uniq = function() {
+//     let newArr = [];
+
+//     for (let i = 0; i < this.length; i++){
+//         if (!newArr.includes(this[i])) {
+//             newArr.push(this[i])
+//         }
+//     }
+//     return newArr;
+// }
+
+Array.prototype.twoSum = function() {
+    let newArr = [];
+
+    for (let i = 0; i < this.length - 1; i++) {
+        // debugger
+        if ((this[i] + this[i + 1]) === 0) {
+            newArr.push([i, i + 1]);
+        };
+    };
+    return newArr;
+}
+
+
+
+Array.prototype.transpose = function() {
+    let transposed = [];
+    // []
+
+    // this = [[2,5],[6,7]]
+
+    for (let i = 0; i < this.length; i++) {
+        transposed.push([]);
+    };
+
+    // [ [], [], ]
+
+    for (let i = 0; i < this.length; i++) {
+        for (let j = 0; j < this.length; j++) {
+            transposed[j].push(this[i][j]);
+        };
+    };
+    return transposed;
+}
+
+
+Array.prototype.myEach = function(cb) {
+
+    for( i = 0; i < this.length - 1; i++) {
+        console.log(cb(this[i]));
+    };
+    
+}
+
+let doubler = function(n) {
+    return 2 * n;
+}
